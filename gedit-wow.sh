94cbbd6c17db7fc21392bc6b8757e79222a24e98
none <none@none.com>
2015-09-16 15:50:21 +0200

    R/PIG lang.xml

---
+++ gedit-wow.sh
@@ -0,0 +1,9 @@
+sudo apt-get install gedit-r-plugin
+
+mkdir -p ~/.local/share/gtksourceview-3.0/language-specs/ && cd ~/.local/share/gtksourceview-3.0/language-specs/
+
+wget https://raw.githubusercontent.com/eyala/gedit-pig/master/gtksourceview-3.0/language-specs/pig.lang
+
+echo "all installed ok! hit enter to quit"
+
+read
